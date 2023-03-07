151b79cdba566aefc136cb6741cf9bd31fc63e0a
Evandro Pomatti <evandro.pomatti@gmail.com>
2023-03-07 12:44:19 -0300

    batch application

---
+++ README.md
@@ -0,0 +1,3 @@
+
+
+https://techcommunity.microsoft.com/t5/azure-paas-blog/the-usage-of-managed-identity-in-the-azure-batch-account-and/ba-p/3607014
--- main.tf
+++ main.tf
@@ -92,3 +92,18 @@ resource "azurerm_batch_account" "main" {
   }
 }
 
+resource "azurerm_role_assignment" "batch" {
+  scope                = azurerm_storage_account.main.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = azurerm_batch_account.main.identity[0].principal_id
+}
+
+resource "azurerm_batch_application" "main" {
+  name                = "molecularanalysis"
+  display_name        = "Molecular Analysis"
+  resource_group_name = azurerm_resource_group.main.name
+  account_name        = azurerm_batch_account.main.name
+  allow_updates       = true
+}
+
+
