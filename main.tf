b9de265dc14bc84a29fb5df379b1a5b7ce435dd2
Evandro Pomatti <evandro.pomatti@gmail.com>
2023-03-15 20:44:59 -0300

    jumpbox

--- main.tf
+++ main.tf
@@ -291,66 +291,66 @@ resource "azurerm_batch_job" "dev" {
 
 ### Administration Jumpbox ###
 
-# resource "azurerm_public_ip" "main" {
-#   name                = "pip-jumpbox-${var.sys}"
-#   resource_group_name = azurerm_resource_group.main.name
-#   location            = azurerm_resource_group.main.location
-#   allocation_method   = "Static"
-# }
+resource "azurerm_public_ip" "main" {
+  name                = "pip-jumpbox-${var.sys}"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  allocation_method   = "Static"
+}
 
-# resource "azurerm_network_interface" "main" {
-#   name                = "nic-jumpbox-${var.sys}"
-#   location            = azurerm_resource_group.main.location
-#   resource_group_name = azurerm_resource_group.main.name
+resource "azurerm_network_interface" "main" {
+  name                = "nic-jumpbox-${var.sys}"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
 
-#   ip_configuration {
-#     name                          = "jumpbox"
-#     subnet_id                     = azurerm_subnet.jumpbox.id
-#     private_ip_address_allocation = "Dynamic"
-#     public_ip_address_id          = azurerm_public_ip.main.id
-#   }
+  ip_configuration {
+    name                          = "jumpbox"
+    subnet_id                     = azurerm_subnet.jumpbox.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.main.id
+  }
 
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-# }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
-# resource "azurerm_linux_virtual_machine" "main" {
-#   name                  = "vm-jumpbox-${var.sys}"
-#   resource_group_name   = azurerm_resource_group.main.name
-#   location              = azurerm_resource_group.main.location
-#   size                  = var.jumpbox_size
-#   admin_username        = var.jumpbox_admin_user
-#   admin_password        = var.jumpbox_admin_password
-#   network_interface_ids = [azurerm_network_interface.main.id]
+resource "azurerm_linux_virtual_machine" "main" {
+  name                  = "vm-jumpbox-${var.sys}"
+  resource_group_name   = azurerm_resource_group.main.name
+  location              = azurerm_resource_group.main.location
+  size                  = var.jumpbox_size
+  admin_username        = var.jumpbox_admin_user
+  admin_password        = var.jumpbox_admin_password
+  network_interface_ids = [azurerm_network_interface.main.id]
 
-#   custom_data = filebase64("${path.module}/cloud-init.sh")
+  custom_data = filebase64("${path.module}/cloud-init.sh")
 
-#   identity {
-#     type = "SystemAssigned"
-#   }
+  identity {
+    type = "SystemAssigned"
+  }
 
-#   admin_ssh_key {
-#     username   = var.jumpbox_admin_user
-#     public_key = file("~/.ssh/id_rsa.pub")
-#   }
+  admin_ssh_key {
+    username   = var.jumpbox_admin_user
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
 
-#   os_disk {
-#     caching              = "ReadWrite"
-#     storage_account_type = "Standard_LRS"
-#   }
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
 
-#   source_image_reference {
-#     publisher = "Canonical"
-#     offer     = "0001-com-ubuntu-server-jammy"
-#     sku       = "22_04-lts-gen2"
-#     version   = "22.04.202303090"
-#   }
-# }
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "22.04.202303090"
+  }
+}
 
-# # Adds permissions to the jumpbox VM to manage Batch
-# resource "azurerm_role_assignment" "jumpbox_batch" {
-#   scope                = azurerm_batch_account.main.id
-#   role_definition_name = "Contributor"
-#   principal_id         = azurerm_linux_virtual_machine.main.identity[0].principal_id
-# }
+# Adds permissions to the jumpbox VM to manage Batch
+resource "azurerm_role_assignment" "jumpbox_batch" {
+  scope                = azurerm_batch_account.main.id
+  role_definition_name = "Contributor"
+  principal_id         = azurerm_linux_virtual_machine.main.identity[0].principal_id
+}
